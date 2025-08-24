7b926d795bdbfffd64f458e675b28513ba2b9e19
nietiene <niyomugaboetiene53@gmail.com>
2025-08-24 15:00:26 -0700

    This is my first commit while I was trying to run my first contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,41 +1,10 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatConfig } from "hardhat/types/config";
+import "@nomicfoundation/hardhat-toolbox";
 
-import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { configVariable } from "hardhat/config";
-
-const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin],
-  solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    },
-  },
+const config: HardhatConfig = {
+  solidity: "0.8.28",
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-    },
+    hardhat: {},
   },
 };
 
