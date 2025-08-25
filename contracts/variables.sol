94b7ee1a72e79f512fb92a7ea9ded1a6fb7c0243
nietiene <niyomugaboetiene53@gmail.com>
2025-08-25 14:50:41 -0700

    this commit includes changes when I was trying to compile my first contract 😎😎😎

--- contracts/variables.sol
+++ contracts/variables.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.28;
 
 contract MyContract {
     uint public myNumber; // here is state variable
+    // public make variable readed by someone else  means outside of the contract anyone reads its value
 
 
     function setNuumber (uint _num) public {
