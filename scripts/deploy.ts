e06268a347a2c7cf7555ebb40fd5f378ddd6e8c3
nietiene <niyomugaboetiene53@gmail.com>
2025-08-30 16:45:08 -0700

    bugs fix

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,13 @@
 import { ethers } from "hardhat";
-
+import { MyContract } from "../typechain-types";
 
 export async function main() {
     const MyContract = await ethers.getContractFactory("MyContract");
-    const myContract: any = await MyContract.deploy();
+    const myContract: any = (await MyContract.deploy()) as MyContract;
 
     await myContract.deployed();
     
-    console.log("Variable contract deployed to:", myContract.address);
+    console.log("Variable contract deployed to:", myContract.target);
 }
 
 main().catch((error) => {
