4334b184674e75a604d148a498dd72b6189b473e
nietiene <niyomugaboetiene53@gmail.com>
2025-08-31 16:14:49 -0700

    ERC20 token

--- scripts/ERC20-int.ts
+++ scripts/ERC20-int.ts
@@ -4,12 +4,12 @@ import { MyTokens } from "../typechain-types";
 export async function main () {
 
     const Address = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
-    const factory = await ethers.getContractFactory("MyContract");
-    const mytoken =  (await factory).attach(Address) as MyTokens;
+    const factory = await ethers.getContractFactory("MyTokens");
+    const mytoken = factory.attach(Address) as MyTokens;
 
     const transfer = await mytoken.transfer("0xcd3B766CCDd6AE721141F452C550Ca635964ce71", 20);
     transfer.wait()
-   console.log("Transfered 20 tokens", transfer);
+    console.log("Transfered 20 tokens!!");
 }
 
 main().catch((error) => {
