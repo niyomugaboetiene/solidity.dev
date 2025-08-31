f72f9bbcb972b5e00067ff992eb42a3a5f47a4d8
nietiene <niyomugaboetiene53@gmail.com>
2025-08-31 16:23:33 -0700

    ERC20 token

--- scripts/ERC20-int.ts
+++ scripts/ERC20-int.ts
@@ -11,7 +11,7 @@ export async function main () {
     // transfer.wait()
     // console.log("Transfered 20 tokens!!");
 
-    const checkBalance = await mytoken.checkBalance("0xcd3B766CCDd6AE721141F452C550Ca635964ce71");
+    const checkBalance = await mytoken.checkBalance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
     console.log("Balance:", checkBalance.toString());
 
 }
