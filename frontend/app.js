c3830daa4ad30389083d3670759984e003758685
nietiene <niyomugaboetiene53@gmail.com>
2025-09-01 16:57:26 -0700

    ERC20 token with metamask and its frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat");
+
 const tokenAddress = "0xE37fBADBaEE85b96f44B0dAeb9044E5cE79C924e";
 const tokenABI = [
     "function name() view returns(string)",
@@ -13,4 +15,27 @@ const tokenABI = [
 let provider;
 let signer;
 let contract;
+
+document.getElementById("connect").onclick = async () => {
+    if (window.ethereum) {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        provider = new ethers.BrowserProvider(window.ethereum);
+        signer = await provider.getSigner();
+        contract = new ethers.Contract(tokenAddress, tokenABI, signer);
+        const account = await signer.getAddress();
+        document.getElementById("account").innerText = "Connected" + account;
+    } else {
+        alert("MetaMask not detected!!");
+    }
+};
+
+// send tokens
+document.getElementById("send").onclick = async () => {
+    const to = document.getElementById("recipient").value;
+    const amount = ethers.parseUnits(document.getElementById("amount").value, 10)
+    const tx = await contract.transfer(to, amount)
+    await tx.wait();
+    alert("Token sent!!");
+};
+
 
