1cc74d3deb7ce8ee11915017b3086ac160ef5516
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 05:12:33 -0700

    frontend: ERC721 commit

--- scripts/ERC721/MyNFT-int.ts
+++ scripts/ERC721/MyNFT-int.ts
@@ -1,2 +1,13 @@
 import { MyNFT } from "../../typechain-types";
 import { ethers } from "hardhat";
+
+export async function main() {
+    const Address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const factory = await ethers.getContractFactory("MyNFT");
+    const myNft = factory.attach(Address) as MyNFT;
+
+
+    const tx = await myNft.MintNFT("0x1CBd3b2770909D4e10f157cABC84C7264073C9Ec");
+    tx.wait();
+    console.log("Amount minted successfully to this address");
+}
