b6708b2e1bb66a0de2cb471c9e6cf1ab270d4535
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 08:41:53 -0700

    ERC721 Owner and tranfer  commit

---
+++ contracts/ERC721/ownerShip&transfer.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Transfer is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
+
+    constructor () ERC721("NF TOKEN", "NFT") Ownable(msg.sender) {
+        tokenCounter = 0;
+    }
+
+    function  mintNFT(address to, string memory tokenURI)  public onlyOwner returns(uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(to, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter += 1;
+        
+        return newTokenId;
+    }
+}
