89d35906900332d165220c9612c8044a30f17b8b
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 06:01:58 -0700

    ERC721 metadata  commit

--- scripts/ERC721/metadata-int.ts
+++ scripts/ERC721/metadata-int.ts
@@ -3,7 +3,7 @@ import { MyNFTMetadata } from "../../typechain-types";
 
 export async function main()  {
     const address = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-    const factory = await ethers.getContractFactory("IERC20Metadata");
+    const factory = await ethers.getContractFactory("MyNFTMetadata");
     const metadata = await factory.attach(address) as MyNFTMetadata;
 
     const tx = await metadata.mintNFT("0xFABB0ac9d68B0B445fB7357272Ff202C5651694a", "https://maroon-abundant-marlin-479.mypinata.cloud/ipfs/bafkreifzu7aayvmpprirwuiowmcpnei4cinrjvp3rhempk3ktm2chd2oqm");
