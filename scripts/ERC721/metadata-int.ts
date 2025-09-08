af552f6f3f189dde888ec3b0411f7026d55a72d4
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 06:10:23 -0700

    ERC721 metadata  commit

--- scripts/ERC721/metadata-int.ts
+++ scripts/ERC721/metadata-int.ts
@@ -9,6 +9,11 @@ export async function main()  {
     const tx = await metadata.mintNFT("0xFABB0ac9d68B0B445fB7357272Ff202C5651694a", "https://maroon-abundant-marlin-479.mypinata.cloud/ipfs/bafkreifzu7aayvmpprirwuiowmcpnei4cinrjvp3rhempk3ktm2chd2oqm");
     tx.wait();
     console.log("Minted succeffully");
+
+    const tokenId = 0;
+    const metadataURI = await metadata.tokenURI(tokenId);
+
+    console.log("Metadata URI:", metadataURI);
 }
 
 main().catch((error) => {
