879ddfebd0c9b2aff100961f030f2f26f7a71013
nietiene <niyomugaboetiene53@gmail.com>
2025-09-07 20:07:23 -0700

    frontend:  ERC721 commit

--- contracts/ERC721/ERC721.sol
+++ contracts/ERC721/ERC721.sol
@@ -4,9 +4,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
+
     uint256 public  tokenCounter;
 
-    constructor () ERC721("MyNFT", "MNFT") {
+    constructor () ERC721("MyNFT", "MNFT") Ownable(msg.sender) {
         tokenCounter = 0;
     }
 
