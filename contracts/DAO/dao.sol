62f6235aebd0de2ce58536cd61c00099000ce221
nietiene <niyomugaboetiene53@gmail.com>
2025-09-12 13:57:01 -0700

    DAO

---
+++ contracts/DAO/dao.sol
@@ -0,0 +1,5 @@
+// DAO is smart contract that allows people to propose action, vote proposal, execute the winning proposals
+// DAO -> Decentralized Autonomous Organization
+// Autonomous -> runs automatically based on rule runs on smart contract
+// Organization -> a group of people with shared goal
+// on ETHereum DAO let people pool money together and vote how to spend it without needing bank 
