9caebd94cfbdf9b99a1f541b64156654d30017d8
nietiene <niyomugaboetiene53@gmail.com>
2025-09-12 14:39:29 -0700

    DAO

--- contracts/DAO/dao.sol
+++ contracts/DAO/dao.sol
@@ -3,3 +3,19 @@
 // Autonomous -> runs automatically based on rule runs on smart contract
 // Organization -> a group of people with shared goal
 // on ETHereum DAO let people pool money together and vote how to spend it without needing bank 
+
+pragma solidity ^0.8.28;
+
+contract SimpleDAO {
+    struct Proposal {
+        
+        string description;
+        uint256 voteCount;
+        bool executed; 
+    }
+
+    Proposal[] public proposals;
+    mapping (address => bool) hasVoted;
+// create proposal
+    function 
+}
