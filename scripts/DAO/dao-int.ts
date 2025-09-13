df41c5f5fa64f49d47930f88e0f5b6c00f3dc040
nietiene <niyomugaboetiene53@gmail.com>
2025-09-13 15:44:45 -0700

    DAO

--- scripts/DAO/dao-int.ts
+++ scripts/DAO/dao-int.ts
@@ -4,7 +4,7 @@ import { SimpleDAO } from "../../typechain-types";
 export async function main() {
     const address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
     const factory = await ethers.getContractFactory("SimpleDAO");
-    const dao = await factory.attach(address) as SimpleDAO;
+    const dao = factory.attach(address) as SimpleDAO;
 
     // const tx = await dao.createProposal("Fund community project");
     // tx.wait();
