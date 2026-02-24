495a688b3eb7c6dfa55a1bb2fb33355c35e90fff
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-24 11:33:39 +0200

    Fixing configuration of hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,9 +12,14 @@ const config: HardhatUserConfig = {
     target: "ethers-v6"
   },
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
+      url: process.env.ALCHEMY_KEY
+        ? `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
+        : "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     }
   }
 };
